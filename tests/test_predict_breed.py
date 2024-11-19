9602d2e2f149336750ae0256f554a54a73582e7b
than-dev <nathan.cotrim@gmail.com>
2024-11-19 19:20:09 -0300

    fix: tests

--- tests/test_predict_breed.py
+++ tests/test_predict_breed.py
@@ -1,26 +1,37 @@
 import unittest
 import os
-from tensorflow.keras.models import load_model
+import tensorflow as tf
+import tf_keras
+import tensorflow_hub as hub
 import numpy as np
 
 class TestPredictBreed(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Carrega o modelo de um dos três arquivos disponíveis na pasta models
+        # Configura o caminho do modelo
         cls.model_path = 'models/20240205-16521707151979-1000-images-mobilenetv2-Adam.h5'
-        cls.model = load_model(cls.model_path)
-
+        
+        # Carrega o modelo com a camada customizada 'KerasLayer'
+        cls.model = tf_keras.models.load_model(cls.model_path, custom_objects={'KerasLayer': hub.KerasLayer})
+    
     def test_predict_breed(self):
-        # Cria uma imagem de teste aleatória para simular a entrada do modelo
-        test_image = np.random.rand(1, 224, 224, 3)  # Ajuste para o tamanho esperado pelo modelo
+        # Simula uma entrada processada para o modelo (imagem pré-processada)
+        test_image = np.random.rand(1, 224, 224, 3).astype(np.float32)  # Array normalizado no intervalo [0, 1]
         predictions = self.model.predict(test_image)
         
         # Verifica se o output está no formato esperado
-        self.assertEqual(predictions.shape[1], 120)  # Exemplo: 120 classes de raças
+        self.assertEqual(predictions.shape[0], 1, "A saída deve ter uma única amostra.")
+        self.assertGreaterEqual(predictions.shape[1], 1, "A saída deve ter pelo menos uma classe.")
+        self.assertAlmostEqual(np.sum(predictions[0]), 1, delta=0.01, msg="As probabilidades devem somar aproximadamente 1.")
 
     def test_model_loaded(self):
-        # Teste para garantir que o modelo foi carregado corretamente
-        self.assertTrue(self.model is not None, "Falha ao carregar o modelo.")
+        # Testa se o modelo foi carregado corretamente
+        self.assertIsNotNone(self.model, "Falha ao carregar o modelo.")
+
+    def test_layer_in_model(self):
+        # Verifica se a camada KerasLayer está no modelo
+        keras_layer_found = any(isinstance(layer, hub.KerasLayer) for layer in self.model.layers)
+        self.assertTrue(keras_layer_found, "O modelo não contém a camada KerasLayer.")
 
 if __name__ == '__main__':
     unittest.main()
